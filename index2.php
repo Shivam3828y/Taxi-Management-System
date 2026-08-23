42f8c4876e7f6c13a6105c48c7b93cd95e491b19
shivam3828y <shivam3828y@gmail.com>
2026-08-23 09:29:08 +0530

    created public homepage

---
+++ index2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Taxi management system</h1>
+
+        <nav>
+            <a href ="index.php">home</a>
+            <a href = "admin/login.php">Admin login</a>
+            <a href ="deriver/login.php">Driver login</a>
+
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h2>welcom to the taxi management system</h2>
+
+            <p>manage taxis , drivers , maintenance, fines and payment in one place</p>
+        </section>
+
+        <section>
+            <h2> out taxis</h2>
+        <p> taxi availability will be displayed here
+        </section>
+    </main>
+    
+    <fppter>
+        <p>&copy; 2026 taxi management system
+    </fppter>
+</body>
+</html>
